2dd3e238e41eaa3966bce0a54ee9749c3d15c699
GDJ <GDJ@DESKTOP-8HNUQMJ>
2024-04-18 16:23:11 +0900

    kmj_Kimmyungjun 클래스 생성

---
+++ semiTest/src/semiTest/Kimmyungjun.java
@@ -0,0 +1,42 @@
+package semiTest;
+
+public class Kimmyungjun {
+	private String name;
+	private int age;
+	private double weight;
+	private char gender;
+	public Kimmyungjun() {
+		// TODO Auto-generated constructor stub
+	}
+	public Kimmyungjun(String name, int age, double weight, char gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+}
