478774830cf3aa891761463b4cb6527753b1a6d2
chs53 <chs53@DESKTOP-NKFMJQF>
2024-04-18 16:47:04 +0900

    chs_animal클래스 수정

--- semiTest/src/semiTest/Animal.java
+++ semiTest/src/semiTest/Animal.java
@@ -3,6 +3,7 @@ package semiTest;
 public class Animal {
 	// 각자 필드를 1개씩 선언하고 메소드를 1개씩 추가로 선언하세요.
 	// 아시겠죠?
+	private String address;
 	private double weight;
 	private String name;
 	private int age;
@@ -14,7 +15,9 @@ public class Animal {
 	public double getWeight() {
 		return weight;
 	}
-
+	public String getAddress() {
+		return this.address;
+	}
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
