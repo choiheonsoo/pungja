bb38f4a0c324f4a0c5f2ec8f67aabd67459ee458
GD <GD@DESKTOP-TOJ0JQP>
2024-04-18 16:39:54 +0900

    gjh_Animal클래스 weight필드 게터세터 추가

--- semiTest/src/semiTest/Animal.java
+++ semiTest/src/semiTest/Animal.java
@@ -3,9 +3,22 @@ package semiTest;
 public class Animal {
 	// 각자 필드를 1개씩 선언하고 메소드를 1개씩 추가로 선언하세요.
 	// 아시겠죠?
-	
+	private double weight;
 	private String name;
+	
 	public String getName() {
 		return this.name;
 	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	
+	
+	
 }
